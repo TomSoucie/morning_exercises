a257eb074bd8ad374c7b033a5f986d81221c64bf
Thomas Soucie <tsoucie1858@gmail.com>
2017-03-20 09:48:33 -0600

    Commits breaks to exit loop

--- leap_year.rb
+++ leap_year.rb
@@ -1,8 +1,8 @@
 
 def leap_year(current)
   leap_count = 0
-  while leap_count < 25
-    (current..2140).each do |year|
+  loop do
+    (current..2220).each do |year|
       if year % 400 == 0
         puts year
         leap_count += 1
@@ -10,8 +10,11 @@ def leap_year(current)
         puts year
         leap_count += 1
       end
+      break if leap_count == 25
     end
+  break  
   end
 end
-  leap_year(2017)
+
+leap_year(2017)
   
