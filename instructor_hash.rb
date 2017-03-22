65c2d6f12fd3aa37809dd0da963ac752eddd7bb6
Thomas Soucie <tsoucie1858@gmail.com>
2017-03-22 09:11:21 -0600

    Initial commit

---
+++ instructor_hash.rb
@@ -0,0 +1,13 @@
+night_shifts = ["josh", "andrew", "sal", "casey", "beth", "ilana", "lauren", "sal", "casey", "casey", "nate", "beth", "nate", "nate", "casey", "casey", "casey", "andrew"]
+
+shift_count = Hash.new
+
+night_shifts.each do |instructor|
+  if shift_count[instructor] == nil
+    shift_count[instructor] = 1
+  else
+    shift_count[instructor] += 1
+  end
+end
+
+puts shift_count
