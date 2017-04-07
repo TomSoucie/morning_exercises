054ed591c8503411d6f01348796a592635380130
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-07 11:44:35 -0600

    Commits for_letters method that checks if there's a word that contains at least one of the letters

--- scrabble/lib/word_finder.rb
+++ scrabble/lib/word_finder.rb
@@ -1,6 +1,13 @@
+require 'pry'
+
 class WordFinder
   def words
     File.read('/usr/share/dict/words').split("\n")
+  end
+
+  def for_letters(letters)
+    letters.map { |letter| words.include? letter }
+    binding.pry
   end
   
 end
--- scrabble/test/word_finder_test.rb
+++ scrabble/test/word_finder_test.rb
@@ -5,5 +5,21 @@ require 'minitest/pride'
 require 'pry'
 
 class WordFinderTest < Minitest::Test
+  def test_it_is_a_wordfinder
+    finder = WordFinder.new
+    assert_instance_of WordFinder, finder 
+  end
 
+  def test_it_can_find_all_the_words
+    finder = WordFinder.new
+
+    assert_equal 141, finder.for_letters(['r', 'e', 'x', 'a', 'p', 't', 'o']).count
+  end
+
+  def test_it_can_find_all_the_words_that_include_a_given_letter
+    skip
+    finder = WordFinder.new
+
+    assert_equal 85, finder.for_letters_with(['e', 'x', 'a', 'p', 't', 'o'], 'r').count
+  end
 end
