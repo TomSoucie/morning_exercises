3db013edd3113613ff8662dee9f0604b683116c4
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-10 09:54:32 -0600

    Commits allergy updates and allergen module

--- allergy/lib/allergy.rb
+++ allergy/lib/allergy.rb
@@ -1,5 +1,8 @@
 class Allergy
+  require Allergens
+
   def initialize(score)
     @score = score
   end
+  
 end
--- allergy/test/allergy_test.rb
+++ allergy/test/allergy_test.rb
@@ -13,6 +13,6 @@ class AllergyTest < Minitest::Test
   def test_score_of_3_returns_egg_and_fish
     a = Allergy.new(3)
 
-
+    assert_equal
   end
 end
